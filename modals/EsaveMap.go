301ab8e4b05fb4d45cd4e2ad3079cee9da51fe64
Florian Krick <florian-krick@web.de>
2024-10-29 21:35:19 +0100

    use new runtime.DrawString function Use new Font System Save files to userDir

--- modals/EsaveMap.go
+++ modals/EsaveMap.go
@@ -13,11 +13,13 @@ import (
 	"os"
 	"outlaw_on_block/runtime"
 	"outlaw_on_block/tiles"
+	"outlaw_on_block/ui"
+	"strings"
 )
 
 type EsaveMap struct {
 	Name        string
-	HoveredChar string
+	InputStatus runtime.FontStatus
 	FileName    string
 	HoverSave   bool
 	TileMap     []tiles.Tile
@@ -39,8 +41,8 @@ func (this *EsaveMap) GetName() string {
 func (this *EsaveMap) Update() error {
 	mouseCursorPosX, mouseCursorPosY := ebiten.CursorPosition()
 	mouseCursorTrigger := image.Rect(mouseCursorPosX, mouseCursorPosY, mouseCursorPosX+1, mouseCursorPosY+1)
-	inputRect := image.Rect(592, 281, 1311, 387)
-	saveBtnTrigger := image.Rect(1265, 791, 1343, 814)
+	inputRect := image.Rect(675, 342, 982, 372)
+	saveBtnTrigger := image.Rect(1260, 792, 1285, 807)
 
 	if mouseCursorTrigger.In(saveBtnTrigger) {
 		this.HoverSave = true
@@ -52,12 +54,12 @@ func (this *EsaveMap) Update() error {
 				return errors.New("Filename was empty")
 			}
 			//1. Create folder named saveName
-			_ = os.Mkdir("save/"+this.FileName, 0755)
+			_ = os.MkdirAll(runtime.OOBUserDir+"/OOB/save/"+this.FileName+"/img", 0755)
 			//2. Create inside img
-			_ = os.Mkdir("save/"+this.FileName+"/img", 0755)
+			//_ = os.Mkdir(runtime.OOBUserDir+"/OOB/save/"+this.FileName+"/img", 0755)
 			//3. Save all tiles to <saveFolder>/img
 			for _, img := range this.TileMap {
-				f, _ := os.Create("save/" + this.FileName + "/img/" + img.ID + ".png")
+				f, _ := os.Create(runtime.OOBUserDir + "/OOB/save/" + this.FileName + "/img/" + img.ID + ".png")
 				defer f.Close()
 				png.Encode(f, img.TileImage)
 			}
@@ -72,7 +74,7 @@ func (this *EsaveMap) Update() error {
 				this.Closed = true
 				return err
 			}
-			manifestFile, _ := os.Create("save/" + this.FileName + "/manifest.json")
+			manifestFile, _ := os.Create(runtime.OOBUserDir + "/OOB/save/" + this.FileName + "/manifest.json")
 			manifestFile.Write(manifestAsByte)
 			this.Closed = true
 			return nil
@@ -82,22 +84,50 @@ func (this *EsaveMap) Update() error {
 	}
 
 	if mouseCursorTrigger.In(inputRect) {
-		/*row := 282
-		cnt := 593
-		for idx, char := range runtime.Alphabet {
-			if idx == 18 {
-				row += 51
-				cnt = 593
+		if this.InputStatus != runtime.FONT_ACTIVE {
+			this.InputStatus = runtime.FONT_HOVER
+		}
+		if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
+			if this.InputStatus == runtime.FONT_ACTIVE {
+				this.InputStatus = runtime.FONT_HOVER
+				log.Println("Set inactive")
+			} else {
+				this.InputStatus = runtime.FONT_ACTIVE
+				log.Println("Set active")
 			}
-			charTrigger := image.Rect(cnt, row, cnt+41, row+51)
-			if mouseCursorTrigger.In(charTrigger) {
-				this.HoveredChar = char
-				if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-					this.FileName += char
+		}
+	} else {
+		if this.InputStatus != runtime.FONT_ACTIVE {
+			this.InputStatus = runtime.FONT_NORMAL
+		}
+	}
+	if this.InputStatus == runtime.FONT_ACTIVE {
+		if keys := inpututil.AppendJustPressedKeys(nil); len(keys) > 0 {
+			for _, key := range keys {
+				if len(key.String()) == 1 {
+					if !ebiten.IsKeyPressed(ebiten.KeyShift) {
+						this.FileName += strings.ToLower(key.String())
+					} else {
+						this.FileName += strings.ToUpper(key.String())
+					}
+				} else {
+					if key == ebiten.KeyBackspace {
+						this.FileName = this.FileName[:len(this.FileName)-1]
+					} else if key == ebiten.KeyMinus {
+						if ebiten.IsKeyPressed(ebiten.KeyShift) {
+							this.FileName += "_"
+						} else {
+							this.FileName += "-"
+						}
+					} else if key == ebiten.KeyEnter {
+						this.InputStatus = runtime.FONT_NORMAL
+					} else if strings.Contains(key.String(), "Digit") {
+						s := strings.Split(key.String(), "Digit")
+						this.FileName += s[1]
+					}
 				}
 			}
-			cnt += 41
-		}*/
+		}
 	}
 	return nil
 }
@@ -112,31 +142,61 @@ func (this *EsaveMap) Draw(screen *ebiten.Image) {
 	}
 
 	vector.DrawFilledRect(screen, relCoods.X, relCoods.Y, 800, 600, color.RGBA{128, 128, 128, 255}, false)
-	runtime.DrawString("Save Map", 1, (int(relCoods.X)+400)-(5*20), int(relCoods.Y)+8, false, screen)
+	runtime.DrawString("Save Map", runtime.FONT_NORMAL, (int(relCoods.X)+400)-(5*20), int(relCoods.Y)+8, screen, &runtime.OOBFontOptions{
+		Colors: struct {
+			Normal color.Color
+			Hover  color.Color
+			Active color.Color
+		}{
+			Normal: color.RGBA{255, 255, 255, 255},
+		},
+		Size: 52,
+	})
 
-	//Draw Clickable Alphabet 18
-	/*row := 0
-	cnt := 0
-	for _, char := range runtime.Alphabet {
-		if cnt == 18 {
-			row++
-			cnt = 0
-		}
-		if char == this.HoveredChar {
-			runtime.DrawString(char, 2, (int(relCoods.X)-270)+(cnt*20), (int(relCoods.Y)-100)+(row*32), true, screen)
-		} else {
-			runtime.DrawString(char, 2, (int(relCoods.X)-270)+(cnt*20), (int(relCoods.Y)-100)+(row*32), false, screen)
-		}
-		cnt++
-	}*/
+	runtime.DrawString("Filename:", runtime.FONT_NORMAL, (int(relCoods.X) + 32), int(relCoods.Y)+100, screen, &runtime.OOBFontOptions{
+		Colors: struct {
+			Normal color.Color
+			Hover  color.Color
+			Active color.Color
+		}{
+			Normal: color.RGBA{255, 255, 255, 255},
+			Hover:  color.RGBA{200, 200, 200, 255},
+			Active: color.RGBA{0, 0, 128, 255},
+		},
+		Size: 24,
+	})
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(5, 1)
+	op.GeoM.Translate((float64(relCoods.X) + 108), float64(relCoods.Y)+100)
+	screen.DrawImage(ui.NewInputUI(), op)
+	runtime.DrawString(this.FileName+".json", this.InputStatus, (int(relCoods.X) + 118), int(relCoods.Y)+100, screen, &runtime.OOBFontOptions{
+		Colors: struct {
+			Normal color.Color
+			Hover  color.Color
+			Active color.Color
+		}{
+			Normal: color.RGBA{128, 128, 128, 255},
+			Hover:  color.RGBA{200, 200, 200, 255},
+			Active: color.RGBA{0, 0, 128, 255},
+		},
+		Size: 24,
+	})
 
-	fileNameString := "Filename: " + this.FileName + ".json"
-	runtime.DrawString(fileNameString, 1, (int(relCoods.X) + 32), int(relCoods.Y)+550, false, screen)
-
+	btnFontOp := &runtime.OOBFontOptions{
+		Colors: struct {
+			Normal color.Color
+			Hover  color.Color
+			Active color.Color
+		}{
+			Normal: color.RGBA{255, 255, 255, 255},
+			Hover:  color.RGBA{200, 200, 200, 255},
+		},
+		Size: 16,
+	}
 	if this.HoverSave {
-		runtime.DrawString("Save", 1, (int(relCoods.X) + 700), int(relCoods.Y)+550, true, screen)
+		runtime.DrawString("Save", runtime.FONT_HOVER, (int(relCoods.X) + 700), int(relCoods.Y)+550, screen, btnFontOp)
 	} else {
-		runtime.DrawString("Save", 1, (int(relCoods.X) + 700), int(relCoods.Y)+550, false, screen)
+		runtime.DrawString("Save", runtime.FONT_NORMAL, (int(relCoods.X) + 700), int(relCoods.Y)+550, screen, btnFontOp)
 	}
 
 }
