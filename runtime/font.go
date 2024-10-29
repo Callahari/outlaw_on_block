33f22d3ab7e4327bb2e1e78c5ca645d27127e1fb
Florian Krick <florian-krick@web.de>
2024-10-29 21:33:13 +0100

    Make runtime.DrawString function more generic

--- runtime/font.go
+++ runtime/font.go
@@ -9,9 +9,26 @@ import (
 	"outlaw_on_block/res"
 )
 
+const (
+	FONT_NORMAL = iota
+	FONT_HOVER
+	FONT_ACTIVE
+)
+
 var (
 	OobFont *text.GoTextFaceSource
 )
+
+type FontStatus int
+
+type OOBFontOptions struct {
+	Colors struct {
+		Normal color.Color
+		Hover  color.Color
+		Active color.Color
+	}
+	Size float64
+}
 
 func init() {
 	s, err := text.NewGoTextFaceSource(bytes.NewReader(res.OOBFont))
@@ -21,16 +38,36 @@ func init() {
 	OobFont = s
 }
 
-func DrawString(s string, size, posX, posY int, hover bool, screen *ebiten.Image) {
+func DrawString(s string, status FontStatus, posX, posY int, screen *ebiten.Image, opt *OOBFontOptions) {
+	if opt == nil {
+		//Use Default settings
+		opt = &OOBFontOptions{
+			Colors: struct {
+				Normal color.Color
+				Hover  color.Color
+				Active color.Color
+			}{
+				Normal: color.RGBA{255, 255, 255, 255},
+				Hover:  color.RGBA{128, 128, 128, 255},
+				Active: color.RGBA{0, 0, 128, 255},
+			},
+			Size: 11,
+		}
+	}
 	op := &text.DrawOptions{}
 	op.GeoM.Translate(float64(posX), float64(posY))
-	if hover {
-		op.ColorScale.ScaleWithColor(color.RGBA{128, 128, 128, 255})
-	} else {
-		op.ColorScale.ScaleWithColor(color.White)
+	switch status {
+	case FONT_NORMAL:
+		op.ColorScale.ScaleWithColor(opt.Colors.Normal)
+	case FONT_HOVER:
+		op.ColorScale.ScaleWithColor(opt.Colors.Hover)
+	case FONT_ACTIVE:
+		op.ColorScale.ScaleWithColor(opt.Colors.Active)
+	default:
+		op.ColorScale.ScaleWithColor(opt.Colors.Normal)
 	}
 	text.Draw(screen, s, &text.GoTextFace{
 		Source: OobFont,
-		Size:   float64(size),
+		Size:   opt.Size,
 	}, op)
 }
